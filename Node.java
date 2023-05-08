100cc7741de8e756fa11c21574eaf55730fd64e7
joehenderson254 <113927729+joehenderson254@users.noreply.github.com>
2023-05-08 11:16:07 -0700

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,18 @@
+public class Node {
+    private int digit;
+    private Node next;
+
+    public Node(int digit) {
+        this.digit = digit;
+        this.next = null;
+    }
+    public Node getNext(){
+        return this.next;
+    }
+    public void setNext(Node node){
+        this.next = node;
+    }
+    public Integer getDigit(){
+        return this.digit;
+    }
+}
